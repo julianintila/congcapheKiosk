72a5a140b52b847f3c7ef4aecb98cd3b6a4b43f2
Macaroni16 <108207923+Macaroni16@users.noreply.github.com>
2025-08-07 11:41:33 +0800

    Create config.php

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+
+return array (
+  'db_server' => '192.168.100.134',
+  'db_name' => 'CONGCAPHEDBNEW',
+  'db_user' => 'sa',
+  'db_pass' => 'PSC@dmin',
+  'printer_names' => 
+  array (
+    0 => 'POS80',
+  ),
+  'register_no' => 1,
+  'queue_prefix' => 'A',
+  'central_server_url' => 'http://192.168.100.134/hikiniku',
+);
+
+?>
